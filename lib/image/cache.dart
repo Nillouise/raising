2794b552e8d3dd891bf298682aa41196b5d9f563
jh.tan <jh.tan@samsung.com>
2020-06-23 21:10:25 +0800

    加入了压缩功能

--- lib/image/cache.dart
+++ lib/image/cache.dart
@@ -1,25 +1,37 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:raising/channel/Smb.dart';
 
+Future<Uint8List> compressImage(Uint8List list) async {
+  var result = await FlutterImageCompress.compressWithList(
+    list,
+    minHeight: 1920,
+    minWidth: 1080,
+    quality: 70,
+  );
+  return Uint8List.fromList(result);
+}
+
 void putImageToCache(String absFilename, int index, Uint8List fileBytes,
-    {bool needFileDetailInfo: false, Smb smb}) async {
+    {bool needFileDetailInfo: false, Smb smb, bool needCompress = true}) async {
   if (smb == null) {
     smb = Smb.getCurrentSmb();
   }
-  await DefaultCacheManager()
-      .putFile([smb.id, absFilename, index].join("@"), fileBytes);
+
+  await DefaultCacheManager().putFile(
+      [smb.id, absFilename, index, needCompress].join("@"),
+      await compressImage(fileBytes));
 }
 
 Future<Uint8List> getImageFromCache(String absFilename, int index,
-    {bool needFileDetailInfo: false, Smb smb}) async {
+    {bool needFileDetailInfo: false, Smb smb, bool needCompress = true}) async {
   if (smb == null) {
     smb = Smb.getCurrentSmb();
   }
   FileInfo fileInfo = await DefaultCacheManager()
-      .getFileFromCache([smb.id, absFilename, index].join("@"));
+      .getFileFromCache([smb.id, absFilename, index, needCompress].join("@"));
   if (fileInfo == null) {
     return null;
   } else {
