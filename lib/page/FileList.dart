d270c0e8d4a9335459d454dcc3fb82e68c7831f1
jh.tan <jh.tan@samsung.com>
2020-07-08 20:43:55 +0800

    修复入口界面

--- lib/page/FileList.dart
+++ lib/page/FileList.dart
@@ -28,12 +28,12 @@ class ExplorerState extends State<Explorer> {
   @override
   Widget build(BuildContext context) {
     SmbNavigation catalog = Provider.of<SmbNavigation>(context);
-//    return FileList();
+
     return new WillPopScope(
         child: Column(
           children: <Widget>[
             Row(
-              children: <Widget>[Text("path:" + catalog.path)],
+              children: <Widget>[Text("path:" + (catalog.path ?? ""))],
             ),
             Expanded(
               child: FileList(),
@@ -77,7 +77,9 @@ class FileListState extends State<FileList> {
   @override
   Widget build(BuildContext context) {
     SmbListModel listModel = Provider.of<SmbListModel>(context);
+    SmbNavigation catalog = Provider.of<SmbNavigation>(context);
     if (listModel.smbs.length == 0) {
+      //处理没有smb的情况
       return Center(
           child: GestureDetector(
         child: Row(
@@ -94,6 +96,49 @@ class FileListState extends State<FileList> {
       ));
 
       return Center();
+    } else if (catalog.smbId?.isEmpty ?? true) {
+      //处理没选SMB的情况
+      SmbListModel smbListModel = Provider.of<SmbListModel>(context);
+      return ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: smbListModel.smbs.length,
+          itemBuilder: (context, index) {
+            final item = smbListModel.smbs[index];
+
+            return Dismissible(
+              // Each Dismissible must contain a Key. Keys allow Flutter to
+              // uniquely identify widgets.
+              key: Key(item.id),
+              // Provide a function that tells the app
+              // what to do after an item has been swiped away.
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  smbListModel.removeSmb(item.id);
+                });
+                // Then show a snackbar.
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text("$item dismissed")));
+              },
+              // Show a red background as the item is swiped away.
+              background: Container(color: Colors.red),
+              child: ListTile(
+                title: Text('${item.id}'),
+                onTap: () {
+                  SmbListModel smbListModel =
+                      Provider.of<SmbListModel>(context, listen: false);
+                  var smb = smbListModel.smbById(item.id);
+                  smb.init();
+                  SmbNavigation smbNavigation =
+                      Provider.of<SmbNavigation>(context, listen: false);
+                  smbNavigation.refresh(context, smb.path, item.id);
+//                smbListModel.
+//                Smb.pushConfig(item.id, hostname, shareName, domain, username, password, path, searchPattern)
+                },
+              ),
+            );
+          });
     } else {
       return FutureBuilder<SmbNavigation>(
         future: () {
